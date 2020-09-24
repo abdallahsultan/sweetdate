1d43358a5bd162ab28907882a2535f812807f340
abdallahsultan <abdallahsultan1996@gmail.com>
2020-09-24 05:53:10 +0200

    cthird

--- resources/views/reserve.blade.php
+++ resources/views/reserve.blade.php
@@ -427,13 +427,13 @@
             <ul class="seats">
                 <li  class="seat">
                 @if(in_array('C7', $tables))
-                <label ><img type="image"  data-toggle="tooltip"   title="C7 @lang('site.reserved')" src="{{asset('website/img/cthird.png')}}"   /> </label>
+                <label ><img type="image"  data-toggle="tooltip"   title="C7 @lang('site.reserved')" src="{{asset('website/img/thirdc.png')}}"   /> </label>
                 <div class="centered"><svg width="3em" height="3em" viewBox="0 0 16 16" class="bi bi-x-square-fill" fill="currentColor" >
                 <path fill-rule="evenodd" d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm3.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"/>
                 </svg></div>
                 @else
                 <input type="checkbox" name="table[C7]" id="C7" />
-                <label for="C7"><img type="image" for="C7" data-toggle="tooltip"   title="C7" src="{{asset('website/img/cthird.png')}}"   /> </label>
+                <label for="C7"><img type="image" for="C7" data-toggle="tooltip"   title="C7" src="{{asset('website/img/thirdc.png')}}"   /> </label>
             
                 @endif
                 </li>
