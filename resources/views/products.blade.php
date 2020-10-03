c7f02c7735e2f5946a97245a8f1a49607de9e257
abdallahsultan <abdallahsultan1996@gmail.com>
2020-10-03 18:58:29 +0200

    sdf

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -120,10 +120,10 @@
                                
                                 
                                 
-                                @if (strlen($product->body) < 100) 
+                                @if (strlen($product->body) < 60) 
                                 <p>{{ strip_tags($product->body) }} </p>
                                 @else
-                                <p>{{  substr($product->body, 0, 100)  }} ... </p>
+                                <p>{{  substr($product->body, 0, 60)  }} ... </p>
                                 @endif
                                 </div>
                             </div>
@@ -152,10 +152,10 @@
                                
                                 
                                 
-                                @if (strlen($product->body) < 100) 
+                                @if (strlen($product->body) < 60) 
                                 <p>{{ strip_tags($product->body) }} </p>
                                 @else
-                                <p>{{  substr($product->body, 0, 100)  }} ... </p>
+                                <p>{{  substr($product->body, 0, 60)  }} ... </p>
                                 @endif
                             </div>
                          </div>
