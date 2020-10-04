fca68edda4f0dc5f15bc7fbdcea376400a7516d2
abdallahsultan <abdallahsultan1996@gmail.com>
2020-10-04 02:28:01 +0200

    font

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -26,6 +26,15 @@
     transform: scale(1.1);
     border: 2px solid #e3cc6fa8; /* (150% zoom - Note: if the zoom is too large, it will go outside of the viewport) */
 }
+.tabsmenu{
+    font-family: mokhfonts !important; 
+      margin-left: 10px;
+         background: linear-gradient(0deg,#ffffff52, transparent, #ffffff52);
+         color: white;
+         border-radius: 12px;
+         font-weight: bold; 
+         font-size: larger;
+}
 /*     
 .flip-box {
   background-color: transparent;
@@ -102,7 +111,7 @@
                       @foreach ($pcategory as $key => $value)
                       
               
-                    <button class="tablinks btn success" style="    margin-left: 10px;   background: linear-gradient(0deg,#ffffff52, transparent, #ffffff52);color: white;border-radius: 12px;font-weight: bold; font-size: larger;"  onclick="openCity(event, {{$value->id}})">{{$value->translate(Config::get('app.locale'))->title}}</button>
+                    <button class="tablinks btn success tabsmenu"  onclick="openCity(event, {{$value->id}})">{{$value->translate(Config::get('app.locale'))->title}}</button>
                          
                        
                     @endforeach
