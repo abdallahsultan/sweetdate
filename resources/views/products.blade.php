5b386f10d054a2ea371d8a1f5f0fe74060948df8
abdallahsultan <abdallahsultan1996@gmail.com>
2020-10-06 12:38:18 +0200

    saf

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -162,12 +162,12 @@
 
 
 
-
+                        
                      </div>
                      
-                  
+                    
                 </div>
-                
+              
               
                
 
@@ -176,6 +176,13 @@
             
                 <!-- slider end -->
 
+            </div>
+            <div class="container">
+            <div class="col-md-12">
+                <p style="color: #e3cc6f;font-weight: bold;font-size: x-large; text-align:center;" <br>.جمبع الأسعار غبر شاملة ضريبة القيمة المضافة
+                     <br>.البيانات الغذائية الإضافية متاحة عند الطلب  
+                     <br> يحتاج الأشخاص البالغون الى معدل  2000 سعره حرارية  يومياوتختلف الاحتياجات من شخص لآخر </p>
+                </div>
             </div>
             <!-- container end -->
         </section>
